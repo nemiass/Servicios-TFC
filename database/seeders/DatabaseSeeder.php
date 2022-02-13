65d02aedd3f1581be0cb28f8ef90b234de2d76a2
nemiias <nehemiastf@gmail.com>
2022-02-13 10:44:23 -0500

    refactor DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call('UsuarioSeeder');
+        $this->call([
+            UsuarioSeeder::class
+        ]);
     }
 }
